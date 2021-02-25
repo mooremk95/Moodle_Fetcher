265486e04258c2fd6d4a768e23877f701293ce3e
Mike <mooremk95@gmail.com>
2021-02-25 15:55:37 -0500

    Completed Course class. set_log has been changed to write_log as has been implemented. fetch_files now downloads files, and calls write_log to update the downloaded.log in the destination directory.

--- Course.py
+++ Course.py
@@ -2,10 +2,12 @@ from pathlib import Path
 from os.path import isdir, isfile
 from os import makedirs
 from re import sub
+from time import sleep
 from Browser import Browser
 
 
 HOME = str(Path.home())
+DOWNLOAD_WAIT_TIME = 1
 
 def prettify_filename(filename):
     filename.replace(" - ", "-").replace(" ", "_")
@@ -45,9 +47,16 @@ class Course():
             self.__set_dir__()
         return isfile(self.__destination + "downloaded.log")
 
-    def __set_log__(self, ):
-        # TODO: This
-        pass
+    def __write_log__(self, filename_set):
+        """Takes a set of filemane strings. Writes a downloaded.log file containing all of these filenmanes as a \n separated list
+        of unicode strings. Overwrites any previously existing downloaded.log file.
+
+        Args:
+            filename_set (set): set of filename strings
+        """
+        with open(self.__destination + "/downloaded.log","w") as f:
+            for filename in filename_set:
+                f.write(f"{filename}\n")
 
     def __restrict_lists__(self, link_lists):
         """Restricts the link_lists dicitonary (in place) to lists of (filename, url link) pairs for which a file of the same 
@@ -62,30 +71,34 @@ class Course():
             link_lists[ext] = [file_pair for file_pair in link_lists[ext] if file_pair[0] not in previous_downloads]
         
 
-    def __download_file__(self, destination, session, file_pair):
+    def __download_file__(self, destination, session, name, url):
         """Given a destination string
 
         Args:
             destination (string): destination folder as a string
             session (requests.Session): requests.session object containing session cookies
-            file_pair (tuple): pair of (filename, url) string pairs. url is the url of the file resource on moodle
+            name (string): filename 
+            url (string): URL to the associated file on moodle
+        Return:
+            boolean: True if successful, False if not
         """
-        inStream = session.get(file_pair[0],stream=True) # open a stream to the PDF
-        filepath = destination + "/" + file_pair[1]
+        inStream = session.get(url, stream=True) # open a stream to the PDF
+        filepath = destination + "/" + name
         if not isdir(destination):
             print(f"Ooops, destination {destination} is not a directory")
             return
         # Check that the stream opened properly
         try:
             inStream.raise_for_status()
+            # open and write to the file
+            chunk = 1024 # let's use 1kb chunks. Why not? 
+            with open(filepath,"wb") as f:
+                for chunk in inStream.iter_content(chunk): # iterate over our stream, writing the bytes
+                    f.write(chunk)
         except Exception as e:
             print(f"Oops, Something went wrong with the download:\n{e}")
-        # open and write to the file
-        chunk = 1024 # let's use 1kb chunks. Why not? 
-        with open(filepath,"wb") as f:
-            for chunk in inStream.iter_content(chunk): # iterate over our stream, writing the bytes
-                f.write(chunk)
-
+            return False
+        return True 
 
     def fetch_files(self):
         """
@@ -98,6 +111,7 @@ class Course():
         # Get the resource file lists form the browser, as well as its session
         file_lists = self.__broswer.fetch_links(self.__url)
         session = self.__broswer.get_session()
+        downloaded_filenames = set()
         if self.__check_log__() and input("Overwrite previously downloaded files? (y/n) ").lower() in ["n", "no"]:
             print("Only downloading files not previously downloaded") 
             self.__restrict_lists__(file_lists) # Restrict the lists of filename,link pairs to those not yet downloaded 
@@ -107,8 +121,15 @@ class Course():
             if not isdir(subdir):
                 makedirs(self.__destination + ext.replace(".", ""))
             # Now loop over the filenames/links to files, and download them
-            for name, url in file_list:
-                print(name)
+            for i, pair in enumerate(file_list):
+                name = pair[0]
+                url = pair[1]
+                print(f"Downloading file {i + 1} of {len(file_list)} {ext} files. {name}")
+                if self.__download_file__(subdir, session, name, url):
+                    downloaded_filenames.add(name)
+                sleep(DOWNLOAD_WAIT_TIME) # Be polite and give moodle a breather between downloads
+        self.__write_log__(downloaded_filenames)
+                
 
 
 
