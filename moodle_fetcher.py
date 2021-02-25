293c20785c3cfa18307c4e3f252374ec359baefd
Mike <mooremk95@gmail.com>
2021-02-25 17:45:07 -0500

    Completed moodle_fetcher.py. The entire script my now be run by running moodle_fetcher.py in a python 3.8+ environment. Selenium, BeatifulSoup4, and requests are required packages.

--- moodle_fetcher.py
+++ moodle_fetcher.py
@@ -12,7 +12,24 @@ def build_courses(browser, course_pair_list):
     Return:
         course_list (list): a list of Course objects
     """
-    return []
+    return [Course(pair[0], pair[1], browser) for pair in course_pair_list]
+
+def get_course_num(course_count, iput):
+    """Gets the course # from user input. Returns -1 if invalid input
+
+    Args:
+        course_count (int): number of courses
+        iput (string): raw user input string
+
+    Returns:
+        int: -1 if invalid user input. Otherwise the course number the user submitted
+    """
+    try:
+        if 0 < int(iput.replace(".","")) <= course_count:
+            return int(iput.replace(".","")) - 1
+    except:
+        return -1
+    return -1
 
 def enumerate_options(course_list):
     """Given a course_list generates a propt string which enumerats the courses the user may download.
@@ -26,12 +43,26 @@ def enumerate_options(course_list):
     options = "Courses:\n"
     for i, course in enumerate(course_list):
         options += f"\t{i+1}: {course.get_name()}\n"
-    options += "Enter a course #, or type \"q\" to quit: "
+    options += "Enter a course number, or type \"q\" to quit: "
     return options 
 
 def main():
     try:
         browser = Browser()
+        # Get the raw course data from the Browser object, then create the course list
+        course_data = browser.fetch_courses()
+        course_list = build_courses(browser, course_data)
+        prompt = enumerate_options(course_list)
+        while 1:
+            iput = input(prompt)
+            if iput.lower() == "q":
+                print("Closing Program")
+                break
+            indx = get_course_num(len(course_list), iput)
+            if indx == -1:
+                print("Ooops, invalid input")
+                continue
+            course_list[indx].fetch_files()
 
     finally: # Always close the browser
         browser.close_browser()    
